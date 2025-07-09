0f66b4bb4bdee17566b75d94bb3d29002e14f850
RafaSilvaVVS <RafaSilvaVVS@users.noreply.github.com>
2025-07-08 21:20:55 -0300

    Validacao de token e links do botao arrumado

---
+++ src/scripts/POST/validarToken.js
@@ -0,0 +1,22 @@
+class validarToken {
+  constructor() {}
+
+  async init() {
+    if (window.localStorage.getItem("token")) {
+      const response = await fetch(
+        "https://ranekapi.origamid.dev/json/jwt-auth/v1/token/validate",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+          },
+        }
+      );
+      const dados = await response.json();
+      console.log(response, dados);
+    }
+  }
+}
+
+export default validarToken;
