e3b310034c01f4e9ec9b5a367a78d7a5b0083a4f
RafaSilvaVVS <RafaSilvaVVS@users.noreply.github.com>
2025-07-25 13:34:03 -0300

    Transacao de produtos concluido e corrigido o bug do formulario

--- src/scripts/POST/criarUsuario.js
+++ src/scripts/POST/criarUsuario.js
@@ -1,6 +1,6 @@
-import Requisicoes from "../endpointsRequisicao/endpoints.js";
-import FazerLogin from "./fazerLogin.js";
-const fazerLogin = new FazerLogin("[data-form-login]", "[data-btn-login]");
+import Requisicoes from '../endpointsRequisicao/endpoints.js';
+import FazerLogin from './fazerLogin.js';
+const fazerLogin = new FazerLogin('[data-form-login]', '[data-btn-login]');
 const PostUsuario = new Requisicoes();
 export let dadosLogin = {};
 class POSTusuario {
@@ -13,35 +13,48 @@ class POSTusuario {
 
   async pegarDadosForm(event) {
     event.preventDefault();
-    if (event.target.name == "email")
+    if (event.target.name == 'email')
       dadosLogin[event.target.name] = event.target.value;
 
-    if (event.target.name == "senha")
+    if (event.target.name == 'senha')
       dadosLogin[event.target.name] = event.target.value;
 
-    this.dados[event.target.name] = event.target.value;
+    this.dados = {
+      nome: this.formulario[0].value,
+      email: this.formulario[1].value,
+      senha: this.formulario[2].value,
+      cep: this.formulario[3].value,
+      numero: this.formulario[4].value,
+      rua: this.formulario[5].value,
+      bairro: this.formulario[6].value,
+      cidade: this.formulario[7].value,
+      estado: this.formulario[8].value,
+    };
   }
 
   async criarUsuario(event) {
     event.preventDefault();
+    console.log(this.dados);
     const response = await PostUsuario.Post(`usuario`, this.dados);
+    console.log(response);
     if (response.message) {
       if (!!this.mensagemDeErro) {
         this.mensagemDeErro.innerText = response.message;
       }
     } else {
       if (!!this.mensagemDeErro) {
-        this.mensagemDeErro.innerText = "";
+        this.mensagemDeErro.innerText = '';
       }
       fazerLogin.FazerLoginUsuarioAutomatico(dadosLogin);
     }
   }
 
   init() {
-    this.formulario?.addEventListener("change", (event) => {
+    this.formulario?.addEventListener('change', (event) => {
       this.pegarDadosForm(event);
     });
-    this.btnCriar?.addEventListener("click", (event) => {
+    this.btnCriar?.addEventListener('click', (event) => {
+      this.pegarDadosForm(event);
       this.criarUsuario(event);
     });
   }
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,71 +1,71 @@
-import getPesquisaProduto from "./GET/getPesquisaProduto.js";
-import produtoUnico from "./GET/getProdutoUnico.js";
+import getPesquisaProduto from './GET/getPesquisaProduto.js';
+import produtoUnico from './GET/getProdutoUnico.js';
 getPesquisaProduto();
-import FormularioAbrir from "./AbrirModal.js";
-import POSTusuario from "./POST/criarUsuario.js";
-import FazerLogin from "./POST/fazerLogin.js";
-import Redirecionar from "./redirecionar/Redirecionar.js";
-import FetchCep from "./GETCep/cep.js";
-import RotaProteger from "./RotaProtegida/ProtegerRota.js";
-import Transacao from "./POST/Transacao.js";
-import DadosUsuario from "./GET/getdados.js";
-import AtualizarDados from "./PUT/atualizarDados.js";
-import Postar from "./POST/produto.js";
-import Compras from "./GET/compras.js";
-import logout from "./logout.js";
+import FormularioAbrir from './AbrirModal.js';
+import POSTusuario from './POST/criarUsuario.js';
+import FazerLogin from './POST/fazerLogin.js';
+import Redirecionar from './redirecionar/Redirecionar.js';
+import FetchCep from './GETCep/cep.js';
+import RotaProteger from './RotaProtegida/ProtegerRota.js';
+import Transacao from './POST/Transacao.js';
+import DadosUsuario from './GET/getdados.js';
+import AtualizarDados from './PUT/atualizarDados.js';
+import Postar from './POST/produto.js';
+import Compras from './GET/compras.js';
+import logout from './logout.js';
 logout();
-import Vendas from "./GET/vendas.js";
-import validarToken from "./POST/validarToken.js";
-import Linkbotao from "./linkBotao.js";
+import Vendas from './GET/vendas.js';
+import validarToken from './POST/validarToken.js';
+import Linkbotao from './linkBotao.js';
 
-const btnLink = new Linkbotao("[data-btn-link]");
+const btnLink = new Linkbotao('[data-btn-link]');
 btnLink.init();
 
 const validar = new validarToken();
 validar.init();
 
-const produtoUnicoInit = new produtoUnico("[data-btn-comprar]");
+const produtoUnicoInit = new produtoUnico('[data-btn-comprar]');
 produtoUnicoInit.init();
 
-const form = new FormularioAbrir("[data-form]", "[data-btn-comprar]");
+const form = new FormularioAbrir('[data-form]', '[data-btn-comprar]');
 form.init();
 
 const postUsuario = new POSTusuario(
-  "[data-form]",
-  "[data-criar]",
-  "[data-erro]"
+  '[data-form-criar]',
+  '[data-criar]',
+  '[data-erro]',
 );
 postUsuario.init();
 
 const fazerLogin = new FazerLogin(
-  "[data-form-login]",
-  "[data-btn-login]",
-  "[data-erro-login]"
+  '[data-form-login]',
+  '[data-btn-login]',
+  '[data-erro-login]',
 );
 fazerLogin.init();
 
 const redirecionar = new Redirecionar();
 
-const cep = new FetchCep("[data-cep]", "[data-change-cep]");
+const cep = new FetchCep('[data-cep]', '[data-change-cep]');
 cep.Init();
 
 const rota = new RotaProteger();
 rota.Rota();
 
 const transacaoInit = new Transacao(
-  "[data-finalizar]",
-  "[data-dados]",
-  "[data-form]"
+  '[data-finalizar]',
+  '[data-dados]',
+  '[data-form]',
 );
 transacaoInit.Init();
 
-const dadosUsuarioInit = new DadosUsuario("[data-dados ]");
+const dadosUsuarioInit = new DadosUsuario('[data-dados]');
 dadosUsuarioInit.Init();
 
-const atualizrDadosConta = new AtualizarDados("[data-btn]", "[data-dados]");
+const atualizrDadosConta = new AtualizarDados('[data-btn]', '[data-dados]');
 atualizrDadosConta.init();
 
-const postProduto = new Postar("[data-btn-postar]", "[data-conteudo]");
+const postProduto = new Postar('[data-btn-postar]', '[data-conteudo]');
 postProduto.init();
 
 const comprasGet = new Compras();
