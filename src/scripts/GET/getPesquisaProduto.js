b824b111b6293cb3b82eb5d1611a6375ad2a5ec5
RafaSilvaVVS <RafaSilvaVVS@users.noreply.github.com>
2025-07-25 13:55:07 -0300

    Pesquisa do produto concluida

--- src/scripts/GET/getPesquisaProduto.js
+++ src/scripts/GET/getPesquisaProduto.js
@@ -1,21 +1,21 @@
-import Requisicoes from "../endpointsRequisicao/endpoints.js";
-import endpoints from "../endpointsRequisicao/endpoints.js";
-import fetchProduto from "./getProdutos.js";
+import Requisicoes from '../endpointsRequisicao/endpoints.js';
+import endpoints from '../endpointsRequisicao/endpoints.js';
+import fetchProduto from './getProdutos.js';
 const getProduto = new Requisicoes();
 export default function getPesquisaProduto() {
-  const pesquisinput = document.querySelector("[data-pesquisa-input]");
+  const pesquisinput = document.querySelector('[data-pesquisa-input]');
   let arrayProdutos = [];
   async function GetPesquisa(valorPesquisa) {
     const produtosPesquisa = await getProduto.GetProdutos(
-      `produto/${valorPesquisa}`
+      `produto?_limit=10&q=${valorPesquisa}`,
     );
     arrayProdutos = produtosPesquisa;
     console.log(arrayProdutos);
     fetchProduto(produtosPesquisa);
   }
   function init() {
-    pesquisinput?.addEventListener("keyup", (event) => {
-      if (event.key == "Enter") {
+    pesquisinput?.addEventListener('keyup', (event) => {
+      if (event.key == 'Enter') {
         GetPesquisa(pesquisinput.value);
       }
     });
